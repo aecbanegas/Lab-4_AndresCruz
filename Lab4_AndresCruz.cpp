03b512cb7fce404c49bd2d153969d14e0815a36d
Andres Cruz <andreselias2000@gmail.com>
2019-05-10 15:02:49 -0600

    Impresion y patron

--- Lab4_AndresCruz.cpp
+++ Lab4_AndresCruz.cpp
@@ -4,8 +4,11 @@
 using namespace std;
 using std::string;
 
-char** crearMatriz(int);
+char** crearMatriz(int,string);
 void liberarMatriz(char**,int);
+void llenarMatriz(char**,int,string);
+void Mazmorra();
+void imprimirMatriz(char**,int,string);
 
 int main(){
 	int opcm=0;
@@ -15,15 +18,83 @@ int main(){
 			<<"2. Salir"<<endl
 			<<"Ingrese una opcion: "<<endl;
 		cin>>opcm;
+		if(opcm==1){
+			Mazmorra();
+		}
 	}	
 }
 
-char** crearMatriz(int size){
+char** crearMatriz(int size, string pfila){
 	char** matriz=new char*[size];
 	for(int i=0;i<size;i++){
-		matriz[i]=new char[size];
+		matriz[i]=new char[pfila.length()];
 	}
 	return matriz;
+}
+
+void Mazmorra(){
+	int size=99;
+	cout<<"Ingrese el numero de filas: "<<endl;
+	cin>>size;
+	while(size<=1){
+		cout<<"Ingrese el numero de filas: "<<endl;
+	        cin>>size;
+	}
+	string pfila;
+	cout<<"Ingrese el patron de la primera fila utilizando ^ y ."<<endl
+		<<"siendo ^ las trampas y . los lugares seguros: "<<endl;
+	cin>>pfila;
+	char** mazmorra= crearMatriz(size,pfila);
+	llenarMatriz(mazmorra,size,pfila);
+	cout<<"La matriz queda: "<<endl;
+	imprimirMatriz(mazmorra,size,pfila);
+	cout<<endl;
+	liberarMatriz(mazmorra,size);
+}
+
+void imprimirMatriz(char** matriz,int size,string pfila){
+	for(int x=0;x<size;x++){
+		for(int y=0;y<pfila.length();y++){
+			cout<<matriz[x][y];
+		}
+		cout<<endl;
+	}
+}
+
+void llenarMatriz(char** matriz, int size, string pfila){
+	for(int i=0;i<pfila.length();i++){
+		matriz[0][i]=pfila[i];
+	}
+	for(int x=1;x<size;x++){
+		bool izq=false,cen=false,der=false;
+		for(int y=0;y<pfila.length();y++){
+			if((y-1)<0){
+				izq=true;
+			}else if(matriz[x-1][y-1]=='.'){
+				izq=true;
+			}else{
+				izq=false;
+			}
+			if((y+1)==pfila.length()){
+				der=true;
+			}else if(matriz[x-1][y+1]=='.'){
+				der=true;
+			}else{
+				der=false;
+			}
+			if(matriz[x-1][y]=='.'){
+				cen=true;
+			}else{
+				cen=false;
+			}
+			if((izq==true&&cen==true&&der==false)||(izq==false&&cen==true&&der==true)||
+					(izq==false&&cen==false&&der==true)||(izq==true&&cen==false&&der==false)){
+				matriz[x][y]='^';
+			}else{
+				matriz[x][y]='.';
+			}
+		}
+	}
 }
 
 void liberarMatriz(char** matriz,int size){
