b576bdbd4deb121e8b0794fd053ce143f18971f7
Andres Cruz <andreselias2000@gmail.com>
2019-05-10 13:25:29 -0600

    Inicio

---
+++ Lab4_AndresCruz.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using std namespace;
+using std::string;
+
+int main(){
+	int opcm=0;
+	while(opcm!=2){
+		cout<<"Menu"<<endl
+			<<"1. Mazmorra"<<endl
+			<<"2. Salir"<<endl
+			<<"Ingrese una opcion: "<<endl;
+		cin>>opcm;
+	}	
+}
+
